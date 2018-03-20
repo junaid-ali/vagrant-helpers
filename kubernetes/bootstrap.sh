5bed00e19b2dad5d107709495daf884991207b39
Junaid Ali <junaidali.yahya@gmail.com>
2018-03-20 13:47:31 +0500

    Updating bootstrap script

--- kubernetes/bootstrap.sh
+++ kubernetes/bootstrap.sh
@@ -27,9 +27,9 @@ sudo sed -i '/netmask/ a\ \ \ \ \ \ gateway 172.19.0.6' /etc/network/interfaces 
 sudo sed -i -- 's/#deb-src/deb-src/g' /etc/apt/sources.list ;
 sudo sed -i -- 's/# deb-src/deb-src/g' /etc/apt/sources.list ;
 IP=$(ip addr | awk '/inet/ && /eth0/{sub(/\/.*$/,"",$2); print $2}') ;
-sudo sed -i -- 's/# eth0 inet dhcp/ eth0 inet static/g' /etc/network/interfaces ;
+sudo sed -i -- 's/ eth0 inet dhcp/ eth0 inet static/g' /etc/network/interfaces ;
 sudo sed -i "/iface eth0 inet static/ a\ \ \ \ \ \ address $IP" /etc/network/interfaces ;
 sudo systemctl stop apt-daily.service ;
 sudo systemctl stop apt-daily.timer ;
 sudo apt-get update ;
-sudo apt-get install software-properties-common python-software-properties -y
+sudo apt-get install software-properties-common python-software-properties vim -y ;
